044ac0995eba34436ca46ba255cbf247ec3f784e
Jan Hoinka <hoinka@gmail.com>
2016-09-08 00:35:46 +0200

    Added and adapted concurrent hash trie map

---
+++ lib/datastructures/concurrent/hash/trie/map/collection/None.java
@@ -0,0 +1,12 @@
+package datastructures.concurrent.hash.trie.map.collection;
+
+/**
+ * Mimic None in Scala
+ *  
+ * @author Roman Levenstein <romixlev@gmail.com>
+ *
+ * @param <V>
+ */
+public class None<V> extends Option<V>{
+
+}
