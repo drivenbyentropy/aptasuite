d9a33e3dcee1e6264b03aa07adb476e7e5f8ffc7
Jan Hoinka <hoinka@gmail.com>
2016-09-17 19:48:21 +0200

    Implemented BitMatrix and serialization utilities

---
+++ src/main/java/Utilities/SaveAndRestore.java
@@ -0,0 +1,52 @@
+package Utilities;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import lib.aptamer.pool.BitMatrix;
+
+/**
+ * @author Jan Hoinka
+ *
+ * Helper functions to serialize and deserialize objects from disk.
+ * This class is applicable to all classes implementing the Serializable interface.
+ */
+public class SaveAndRestore {
+
+	/**
+	 * Serialize the current instance of the object to disk. This is useful for 
+	 * later restoring an existing project from disk. 
+	 * @param fileName
+	 * @throws IOException
+	 */
+	public void serialize(Object obj, String fileName) throws IOException {
+		
+		FileOutputStream fos = new FileOutputStream(fileName);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(obj);
+
+		fos.close();
+	}
+	
+	/**
+	 * Deserialize the a previous instance of the class.
+	 * @param fileName the path of the file at which the object is stored
+	 * @return and instance of the deserialized Object
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	private Object deserialize(String fileName) throws IOException, ClassNotFoundException {
+
+		FileInputStream fis = new FileInputStream(fileName);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Object obj = ois.readObject();
+		ois.close();
+		
+		return obj;
+		
+	}	
+	
+}
---
+++ src/main/java/lib/aptamer/pool/BitMatrix.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package lib.aptamer.pool;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.BitSet;
+
+/**
+ * @author Jan Hoinka
+ * 
+ * This class implements a two dimensional bit array based on a one dimensional bit array.
+ * The indices are transparently translated from 2D to 1D. This allows for space efficient
+ * storage of binary data.
+ */
+public class BitMatrix implements Serializable{
+	
+	/**
+	 * UID for Serializable interface
+	 */
+	private static final long serialVersionUID = 8760015085362970029L;
+
+
+	/**
+	 * The BitSet storing the matrix data.
+	 */
+	private BitSet data = null;
+	
+	
+	/**
+	 * The number of rows of the matrix
+	 */
+	private int rows = 0;
+	
+	
+	/**
+	 * The number of columns of the matrix
+	 */
+	private int cols = 0;
+	
+	
+	/**
+	 * Initialize the BitMatrix in a "row major" manner. All values are initially set to false.
+	 * @param rows number of rows
+	 * @param cols number of columns
+	 */
+	public BitMatrix(int rows, int cols){
+		
+		if (rows <= 0 || cols <= 0){
+			throw new IndexOutOfBoundsException("The number of rows (" + rows + ") or columns (" + cols + ") is invalid");
+		}
+		
+		data = new BitSet(rows*cols);
+		this.rows = rows;
+		this.cols = cols;
+		
+	}
+	
+	
+	/**
+	 * Sets the cell corresponding  to (<code>row</code>,<code>col</code>) of the matrix to <code>value</code>.
+	 * @throws IndexOutOfBoundsException If row or column exceeds the matrix dimension.
+	 * @param row
+	 * @param col
+	 * @param value
+	 */
+	public void set(int row, int col, boolean value){
+		
+		// Boundary check
+		if ( (row < 0 && row >= rows) || (col < 0 && col >= cols) ){
+			
+			throw new IndexOutOfBoundsException("The matrix of size (" + rows + "," + cols + ") cannot be accessed at (" + row + "," + col + ")");
+			
+		}
+		
+		data.set(cols * row + col , value);
+		
+	}
+	
+	/**
+	 * Gets the values of the cell corresponding  to (<code>row</code>,<code>col</code>).
+	 * @throws IndexOutOfBoundsException If row or column exceeds the matrix dimension.
+	 * @param row
+	 * @param col
+	 * @param value
+	 */
+	public boolean get(int row, int col){
+		
+		// Boundary check
+		if ( (row < 0 && row >= rows) || (col < 0 && col >= cols) ){
+			
+			throw new IndexOutOfBoundsException("The matrix of size (" + rows + "," + cols + ") cannot be accessed at (" + row + "," + col + ")");
+			
+		}
+		
+		return data.get(cols * row + col);
+		
+	}
+	
+	
+	/**
+	 * Resets all values of the matrix to its initial state (false).
+	 */
+	public void clear(){
+		
+		data.clear();
+	
+	}
+	
+}
